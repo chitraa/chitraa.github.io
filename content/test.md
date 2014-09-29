be92f2eb2659d0d8ca16e51bf0954d432c7007bb
chitraa <chits.26@gmail.com>
2014-09-29 19:00:03 +0530

    creating first helloworld post

---
+++ content/test.md
@@ -0,0 +1,10 @@
+Title: Hello World
+Date: 2014-09-29
+Category: Hello World
+Tags: test, helloWorld, blogging
+Slug: my-first-post
+Author: Me
+Summary: My first blog using pelican
+
+Hi there! I shall quote [Linus Torvalds](http://en.wikipedia.org/wiki/Linus_Torvalds)
+> "In real open source, you have the right to control your own destiny."
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Chitra Singh'
+SITENAME = u'My personal blog'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+DISQUS_SITENAME = "chitrasingh"
+GOOGLE_ANALYTICS = "UA-55242332-1"
+
+# Blogroll
+'''
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+         )
+'''
+
+# Social widget
+
+SOCIAL = (('twitter', 'http://twitter.com/chit_raa'),
+          ('github', 'http://github.com/chitraa'),)
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "/home/chitra/code/7thsem/aost/pelican-twitchy"
+
+GITHUB_URL = 'http://github.com/chitraa/'
