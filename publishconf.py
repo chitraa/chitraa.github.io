3a8084bdb7b8cb0de21bcc75b3a278ec8c2410f1
chitraa <chits.26@gmail.com>
2014-09-29 23:49:33 +0530

    corrected SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://chitraa.github.com'
+SITEURL = 'http://chitraa.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
